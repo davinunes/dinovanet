8d9f6ea797f75f1ccf588f9a42bd4a394b6dcaf1
ilunne <davi.nunes@gmail.com>
2026-01-29 22:46:52 -0300

    começou

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["https://book.digitalinovation.com.br", "http://localhost:5173"],
+        methods: ["GET", "POST"]
+    },
+    path: "/socket.io/"
+});
+
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+    });
+});
+
+const PORT = 3000;
+httpServer.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
